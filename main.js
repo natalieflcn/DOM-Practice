9f3087f88b604b9c26149c197fd6f1544e101354
Natalie <natalie.dflcn@gmail.com>
2023-11-27 19:35:39 -0500

    Added remove song functionality

--- main.js
+++ main.js
@@ -26,14 +26,27 @@ const addSong = e => {
 
     //Appends delete button to the <li> element
     li.appendChild(delBtn);
-    
+
     //Adds song into song list
     songList.appendChild(li);
     
 
 }
 
+const removeSong = e => {
+    //Makes sure this function only runs when the "delete" button is clicked, not the rest of the list
+    if(e.target.classList.contains('delete')){ //Focuses on the delete buttons by targeting one of its classes
+        if(confirm('Are you sure you want to delete this song?')) { //A confirmation box will appear for the user to confirm action
+            var li = e.target.parentElement; //This sets our li variable to the actual song being removed (the parent of the delete button is the li)
+            songList.removeChild(li); //Removes the li from the ul element (songList) by passing in the variable "li" (the song being removed)
+        }
+    }
+}
+
 //Form Submit Event
 form.addEventListener('submit', addSong);
+
+//Delete Event
+songList.addEventListener('click', removeSong);
 
 
